f4622766d80e535aded02ce1d487c70e50944b87
Stela Capsa <stela.capsa@gmail.com>
2019-11-16 22:42:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |message| 
   puts message
   assign_rooms(room_assignments).each do |rooms|
-    puts array1
+    puts rooms
 end
 end
 end

b7a620aebb920d9f876b0f3bc93f4365dddb8edb
Stela Capsa <stela.capsa@gmail.com>
2019-11-16 21:59:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name = "Arel")
+  return "Hello, my name is #{name}."
+end
+
+   def batch_badge_creator(attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]) 
+     attendees.each do |badge| 
+     puts "Hello, my name is #{badge}."
+end
+
+ end
+
+
+
+ def assign_rooms(room_assignments)
+      room_assignments = 0
+      attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+          puts "Hello, #{attendees}! You'll be assigned to room #{room_assignments}!"
+          room_assignments +=1
+        end
+      
+      
+
+   
+
